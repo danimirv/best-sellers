03b5814767e0b3598f37eff20f8752a791082535
Pablo Gutierrez <gutmox@gmail.com>
2020-08-04 22:14:26 +0200

    Just some cosmetics

--- src/main/java/com/masmovil/best_sellers/MainVerticle.java
+++ src/main/java/com/masmovil/best_sellers/MainVerticle.java
@@ -1,27 +1,21 @@
 package com.masmovil.best_sellers;
 
-import io.vertx.reactivex.core.AbstractVerticle;
-import io.vertx.reactivex.core.http.HttpServer;
-
-import java.util.Arrays;
-
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.masmovil.best_sellers.model.BestSellerRequest;
 import com.masmovil.best_sellers.model.ErrorMessage;
-import com.masmovil.best_sellers.model.TopTenUpdate;
 import com.masmovil.best_sellers.repositories.ItemRepository;
-import com.masmovil.best_sellers.util.ItemUtil;
-
 import io.reactivex.Completable;
 import io.reactivex.Single;
 import io.vertx.core.json.Json;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
+import io.vertx.reactivex.core.AbstractVerticle;
+import io.vertx.reactivex.core.http.HttpServer;
 import io.vertx.reactivex.ext.web.Router;
 import io.vertx.reactivex.ext.web.RoutingContext;
 import io.vertx.reactivex.ext.web.handler.BodyHandler;
+import java.util.Arrays;
 
 public class MainVerticle extends AbstractVerticle {
 
@@ -37,18 +31,18 @@ public class MainVerticle extends AbstractVerticle {
 	private final ItemRepository itemRepository;
 
 	public MainVerticle() {
-	        this(new ItemRepository(), new ObjectMapper());
+		this(new ItemRepository(), new ObjectMapper());
 	}
 
 	public MainVerticle(ItemRepository itemRepository, ObjectMapper mapper) {
-	        this.itemRepository = itemRepository;
+		this.itemRepository = itemRepository;
 		this.mapper = mapper;
 	}
 
 	@Override
 	public Completable rxStart() {
 		vertx.exceptionHandler(error -> LOGGER.info(error.getMessage() + error.getCause()
-				+ Arrays.toString(error.getStackTrace()) + error.getLocalizedMessage()));
+			+ Arrays.toString(error.getStackTrace()) + error.getLocalizedMessage()));
 		return createRouter().flatMap(router -> startHttpServer(HOST, PORT, router)).flatMapCompletable(httpServer -> {
 			LOGGER.info("HTTP server started on http://{0}:{1}", HOST, PORT.toString());
 			return Completable.complete();
@@ -76,7 +70,7 @@ public class MainVerticle extends AbstractVerticle {
 
 	private void reponseErrorMessage(RoutingContext context) {
 		context.response().setStatusCode(400).putHeader("content-type", "application/json")
-				.end(Json.encode(new ErrorMessage("update_time cannot be resolved to a type")));
+			.end(Json.encode(new ErrorMessage("update_time cannot be resolved to a type")));
 	}
 
 	private BestSellerRequest getRequest(RoutingContext context) throws JsonProcessingException {
