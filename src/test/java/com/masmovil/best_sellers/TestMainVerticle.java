b1c58725f233096bcf1783dcc2a72c9a51607201
unknown <usuario@GFI12400.gfi.es>
2020-08-04 10:31:58 +0200

    TEsting end point

--- src/test/java/com/masmovil/best_sellers/TestMainVerticle.java
+++ src/test/java/com/masmovil/best_sellers/TestMainVerticle.java
@@ -1,8 +1,10 @@
 package com.masmovil.best_sellers;
 
 import io.reactivex.Single;
+import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
 import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
 import io.vertx.ext.unit.junit.RunTestOnContext;
@@ -11,9 +13,12 @@ import io.vertx.ext.unit.junit.VertxUnitRunner;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import java.io.IOException;
+import java.net.ServerSocket;
 import java.time.LocalDateTime;
 import java.util.Arrays;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -41,10 +46,18 @@ public class TestMainVerticle {
 
 	private ObjectMapper mapper = new ObjectMapper();
 
+	private Vertx vertx;
+	int port;
+
 	@Before
-	public void deploy_verticle(TestContext testContext) {
-		Vertx vertx = rule.vertx();
-		vertx.deployVerticle(new MainVerticle(), testContext.asyncAssertSuccess());
+	public void deploy_verticle(TestContext testContext) throws IOException {
+		vertx = rule.vertx();
+		ServerSocket socket = new ServerSocket(0);
+		socket.close();
+		port = socket.getLocalPort();
+		DeploymentOptions options = new DeploymentOptions().setConfig(new JsonObject().put("http.port", port));
+		vertx.deployVerticle(new MainVerticle(), options, testContext.asyncAssertSuccess());
+		// vertx.deployVerticle(new MainVerticle(), testContext.asyncAssertSuccess());
 		itemRepository = mock(ItemRepository.class);
 	}
 
@@ -60,6 +73,17 @@ public class TestMainVerticle {
 		givenTopTenItemsInfoWorks(bsr);
 	}
 
+	@Test
+	public void testMyApplication(TestContext context) {
+		final Async async = context.async();
+		vertx.createHttpClient().getNow(port, "localhost", "/best-sellers/top-ten", response -> {
+			response.handler(body -> {
+				context.assertTrue(body.toString().contains("{}"));
+				async.complete();
+			});
+		});
+	}
+
 	private void givenTopTenItemsInfoWorks(BestSellerRequest bsr) throws JsonProcessingException {
 
 		Item item = new Item(1).withType(ItemType.EBOOK).withDescription("Ebook lord of the ring")
@@ -68,5 +92,10 @@ public class TestMainVerticle {
 		String json = mapper.writeValueAsString(Arrays.asList(item));
 		when(itemRepository.topTen(bsr)).thenReturn(Single.just(new JsonArray(json)));
 
+	}
+
+	@After
+	public void tearDown(TestContext context) {
+		vertx.close(context.asyncAssertSuccess());
 	}
 }
